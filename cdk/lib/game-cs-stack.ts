c4e6f406f353964967c540efa60e54d42e43ff15
JimmyChen <15362450@qq.com>
2026-04-06 12:45:24 +0000

    fix: use account-specific KB name to avoid orphaned resource conflicts

--- cdk/lib/game-cs-stack.ts
+++ cdk/lib/game-cs-stack.ts
@@ -199,7 +199,7 @@ export class GameCsAgentStack extends cdk.Stack {
     const knowledgeBase = new cdk.CustomResource(this, 'KnowledgeBase', {
       serviceToken: knowledgeBaseProvider.serviceToken,
       properties: {
-        KnowledgeBaseName: 'game-cs-kb',
+        KnowledgeBaseName: `game-cs-kb-${this.account}`,
         RoleArn: bedrockKbRole.roleArn,
         BucketArn: kbBucket.bucketArn,
         Region: this.region,
